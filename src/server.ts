c871cbfc31516f0d27e87b02d0ac741cc197a127
Lucas Saliés Brum <sistematico@gmail.com>
2024-02-23 16:23:03 -0400

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { Hono } from 'hono'
 import { serveStatic } from '@hono/node-server/serve-static'
-import { createWriteStream } from 'fs'
+import { appendFile } from 'node:fs/promises'
+import { join } from 'node:path'
 
 const app = new Hono()
 
-async function concatChunks() {
-  
-}
+const UPLOAD_DIR = join(import.meta.dir, '..', 'public', 'uploads')
 
 app.use('/*', serveStatic({ root: '/public' }))
 
@@ -15,17 +14,16 @@ app.post('/upload', async (c) => {
   const filename = c.req.header('filename') // parse
   const file = body['file']
 
+  const uploadDir = UPLOAD_DIR + '/' + filename
+  
   if (!filename) return c.text('No filename was passed.') // Check if anything was passed as 'file'
   if (!file) return c.text('No file was passed in.') // Check if anything was passed as 'file'
   if (!(file instanceof File)) return c.text('That is not a file.') // Check if its a file
 
-  const stream = createWriteStream(import.meta.dir + '/' + filename) // Where to save - process.cwd().replace(/\\/g, '/')+'/'+file.name
-  stream.on('error', _ => c.text('Error writing to file.')) // Error Handling
-  
-  const buffer = Buffer.from(await file.arrayBuffer()) // buffering the buffer? idk
-  stream.write(buffer)
-  
-  stream.end()
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
+
+  await appendFile(uploadDir, buffer)
 
   return c.html('File has been uploaded!')
 })
